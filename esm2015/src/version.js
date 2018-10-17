de3e94fbac7023432ef42b39cdcf8fe586b2ded1
Pete Bacon Darwin <pete@bacondarwin.com>
2018-10-17 13:59:09 +0200

    1c561a833c perf(ivy): ngcc - only render .d.ts analysis when necessary

---
+++ esm2015/src/version.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Version } from '@angular/core';
+/** @type {?} */
+export const VERSION = new Version('7.0.0-rc.1-1c561a833c');
+//# sourceMappingURL=version.js.map
